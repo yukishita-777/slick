8fee8968efadb913b2e021b16fdbdf3b74edbcbf
EC2 Default User <ec2-user@ip-172-31-11-69.ap-northeast-1.compute.internal>
2020-12-15 14:08:50 +0000

    課題提出

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slide').slick({
+  autoplay: true,
+  infinite: true,
+  speed: 2000,
+  prevArrow: '<div class="slide-arrow prev-arrow"><img src="images/prev.png"></div>',
+  nextArrow: '<div class="slide-arrow next-arrow"><img src="images/next.png"></div>',
+  dots: true,
+  dotsClass: 'slick-dots'
+});
